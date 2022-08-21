0af41e33bb721834124fde5f7d4dc29448569935
Asif Ahammed <ahammedasif06@gmail.com>
2022-08-21 09:33:10 +0600

    last

--- js/transaction.js
+++ js/transaction.js
@@ -4,21 +4,22 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositAmount = parseFloat(depositValue)
     depositField.value = '';
 
-    if (typeof depositAmount != 'number') {
+    if (isNaN(depositAmount)) {
         alert('Insert a number')
+        return;
 
     }
-    else {
-        const deposit = document.getElementById('deposit-value');
-        const previousDeposit = parseFloat(deposit.innerText);
-        const newDeposit = previousDeposit + depositAmount;
-        deposit.innerText = `${newDeposit}`;
-        const balanceField = document.getElementById('balance-value');
-        const balance = balanceField.innerText;
-        const balanceAmount = parseFloat(balance);
-        const totalBalance = depositAmount + balanceAmount;
-        balanceField.innerText = totalBalance;
-    }
+
+    const deposit = document.getElementById('deposit-value');
+    const previousDeposit = parseFloat(deposit.innerText);
+    const newDeposit = previousDeposit + depositAmount;
+    deposit.innerText = `${newDeposit}`;
+    const balanceField = document.getElementById('balance-value');
+    const balance = balanceField.innerText;
+    const balanceAmount = parseFloat(balance);
+    const totalBalance = depositAmount + balanceAmount;
+    balanceField.innerText = totalBalance;
+
 
 
 })
@@ -28,15 +29,25 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawAmount = withdrawValue.value;
     const withdrawAmountNumber = parseFloat(withdrawAmount);
     withdrawValue.value = '';
+    if (isNaN(withdrawAmountNumber)) {
+        alert('Insert a number')
+        return;
+    }
+
     const previousWithdraw = document.getElementById('withdraw-value');
     const previousWithdrawNumber = parseFloat(previousWithdraw.innerText);
-    const withdrawTotal = previousWithdrawNumber + withdrawAmountNumber;
-    console.log(withdrawTotal);
-    previousWithdraw.innerText = withdrawTotal;
+
+
     const balanceField = document.getElementById('balance-value');
     const balance = balanceField.innerText;
     const balanceAmount = parseInt(balance);
-    const totalBalance = balanceAmount - withdrawAmount;
+    if (withdrawAmountNumber > balanceAmount) {
+        alert('no more money');
+        return;
+    }
+    const withdrawTotal = previousWithdrawNumber + withdrawAmountNumber;
+    previousWithdraw.innerText = withdrawTotal;
+    const totalBalance = balanceAmount - withdrawAmountNumber;
     balanceField.innerText = totalBalance;
 
 
